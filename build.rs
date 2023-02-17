756017f3c847e388b8b245357dd6c7ee7e755b97
Andy-Python-Programmer <andypythonappdeveloper@gmail.com>
2023-02-17 18:41:09 +1100

    build.rs: clone the resources in OUT_DIR
    Signed-off-by: Andy-Python-Programmer <andypythonappdeveloper@gmail.com>

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::path::PathBuf;
+use std::path::Path;
 use std::process::Command;
 
 // lai/meson.build - sources
@@ -27,10 +27,11 @@ const SOURCES: &[&str] = &[
 const LAI_GITHUB_URL: &str = "https://github.com/managarm/lai";
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let lai_path_str = String::from("bundled/lai");
+    let out_dir = std::env::var_os("OUT_DIR").unwrap();
+    let lai_path = Path::new(&out_dir).join("bundled").join("lai");
+    let lai_path_str = lai_path.to_string_lossy();
 
-    if !PathBuf::from(lai_path_str.clone()).exists() {
-        // If we have not already downloaded the source, do so now.
+    if !lai_path.exists() {
         Command::new("git")
             .args(&["clone", LAI_GITHUB_URL, &lai_path_str])
             .status()
