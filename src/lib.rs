a6b2554cfd6e6fd12d8301b4cb8ad8aa0178cb4d
Andy-Python-Programmer <andypythonappdeveloper@gmail.com>
2023-02-17 18:24:26 +1100

    lai: pci_route_pin
    Signed-off-by: Andy-Python-Programmer <andypythonappdeveloper@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,31 @@ mod helper;
 mod host;
 mod raw;
 
+use alloc::boxed::Box;
 pub use host::*;
+pub use raw::*;
+
+#[derive(Debug, Copy, Clone)]
+#[repr(i32)]
+pub enum Error {
+    None,
+    OutOfMemory,
+    TypeMismatch,
+    NoSuchNode,
+    OutOfBounds,
+    ExecutionFailure,
+    IllegalArguments,
+
+    /// Evaluating external inputs (e.g., nodes of the ACPI namespace) returned an unexpected result.
+    /// Unlike LAI_ERROR_EXECUTION_FAILURE, this error does not indicate that
+    /// execution of AML failed; instead, the resulting object fails to satisfy some
+    /// expectation (e.g., it is of the wrong type, has an unexpected size, or consists of
+    /// unexpected contents)
+    UnexpectedResult,
+    /// Error given when end of iterator is reached, nothing to worry about.
+    EndReached,
+    NotSupported,
+}
 
 extern crate alloc;
 
@@ -38,4 +62,31 @@ pub fn enter_sleep(sleep_state: u8) {
 
 pub fn reset() {
     unsafe { assert_eq!(raw::lai_acpi_reset(), raw::LAI_SUCCESS) }
+}
+
+pub fn pci_route_pin(
+    seg: u16,
+    bus: u8,
+    slot: u8,
+    function: u8,
+    pin: u8,
+) -> Result<Box<AcpiResource>, Error> {
+    let mut dest = Box::new(AcpiResource::default());
+    unsafe {
+        let result = raw::lai_pci_route_pin(
+            &mut *dest as *mut AcpiResource,
+            seg,
+            bus,
+            slot,
+            function,
+            pin,
+        );
+
+        if result != LAI_SUCCESS {
+            let err: Error = core::mem::transmute(result);
+            Err(err)
+        } else {
+            Ok(dest)
+        }
+    }
 }
