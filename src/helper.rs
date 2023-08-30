406e271e5ae24390cce50165ea1cdd4841e308e4
Gus Caplan <me@gus.host>
2023-08-29 20:32:45 -0700

    many changes

--- src/helper.rs
+++ src/helper.rs
@@ -1,3 +1,5 @@
+use core::ffi::c_char;
+
 /// Creates a Rust string from the provided C string.
 ///
 /// ## Safety
@@ -7,9 +9,9 @@
 /// of `ptr`.
 /// - It is not guaranteed that the memory pointed by `ptr` won’t change
 /// before the Rust string has been destroyed.
-pub(crate) unsafe fn c_str_as_str<'cstring>(ptr: *const u8) -> &'cstring str {
+pub(crate) unsafe fn c_str_as_str<'cstring>(ptr: *const c_char) -> &'cstring str {
     let length = c_strlen(ptr);
-    let slice = core::slice::from_raw_parts(ptr, length);
+    let slice = core::slice::from_raw_parts(ptr as _, length);
 
     core::str::from_utf8_unchecked(slice)
 }
@@ -19,7 +21,7 @@ pub(crate) unsafe fn c_str_as_str<'cstring>(ptr: *const u8) -> &'cstring str {
 /// ## Safety
 /// - The provided pointer must be valid.
 /// - The provided pointer must point to a null-terminated C string.
-pub(crate) unsafe fn c_strlen(mut ptr: *const u8) -> usize {
+pub(crate) unsafe fn c_strlen(mut ptr: *const c_char) -> usize {
     let mut length = 0;
 
     while *ptr != 0 {
